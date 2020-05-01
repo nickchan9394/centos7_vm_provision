8ee1d6122c85620082bf15dcb10d8a7eaf4f5599
NickChan9394 <nickchan9394@live.cn>
2020-05-01 13:22:29 +0800

    Log out put for gnome installation

--- manifests/modules/gnome/manifests/init.pp
+++ manifests/modules/gnome/manifests/init.pp
@@ -3,12 +3,13 @@ class gnome (
 ) {
 
   exec { 'Install GNOME' :
-    command  => 'yum groupinstall "GNOME Desktop" -y && systemctl set-default graphical.target',
-    path     => '/usr/bin:/usr/sbin:/bin',
-    provider => 'shell',
-    onlyif   => 'test `yum list installed | grep gnome | wc -l` -eq 0',
-    timeout  => 0,
-    user     => 'root',
+    command   => 'yum groupinstall "GNOME Desktop" -y && systemctl set-default graphical.target',
+    path      => '/usr/bin:/usr/sbin:/bin',
+    provider  => 'shell',
+    onlyif    => 'test `yum list installed | grep gnome | wc -l` -eq 0',
+    timeout   => 0,
+    user      => 'root',
+    logoutput => true,
   }
 
 }
